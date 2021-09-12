cce90b4715b757e508fa7e6cb5b8f8a7eeb91353
akinobu <siguiente.football.373@gmail.com>
2021-09-12 15:50:57 +0900

    配列にSwift追加

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class LanguageController extends Controller
 {
         public function index () {
-        $languages = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Ruby', 'Python'];
+        $languages = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Ruby', 'Python','Swift'];
         return view('language.index', ['languages' => $languages]);
     }
 }
