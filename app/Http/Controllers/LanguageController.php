25e69006433f223c264bbf9f8eef47780796d55b
akinobu <siguiente.football.373@gmail.com>
2021-09-12 14:11:16 +0900

    PHPを追加

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class LanguageController extends Controller
 {
     public function index () {
-    $languages = ['HTML', 'CSS', 'JavaScript'];
+    $languages = ['HTML', 'CSS', 'JavaScript','PHP'];
     return view('language.index', ['languages' => $languages]);
     }
 }
